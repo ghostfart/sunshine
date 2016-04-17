03d82534f585dce03cfa37587a0c841df20e14aa
ghostfart <g.b.maitland@gmail.com>
2016-04-17 12:03:55 +1200

    Add Fake Data to MainActivityFragment.java

--- app/src/main/java/nz/co/maitech/sunshine/MainActivityFragment.java
+++ app/src/main/java/nz/co/maitech/sunshine/MainActivityFragment.java
@@ -17,6 +17,13 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        String fakeData[] = new String[6];
+        fakeData[0] = new String("Today - Sunny - 88 / 63");
+        fakeData[1] = new String("Tomorrow - Foggy - 70 / 46");
+        fakeData[2] = new String("Wednesday - Cloudy - 72 / 63");
+        fakeData[3] = new String("Thursday - Rainy - 64 / 51");
+        fakeData[4] = new String("Friday - Foggy - 70 / 46");
+        fakeData[5] = new String("Saturday - Sunny - 76 / 68");
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
