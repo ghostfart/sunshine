a0b39aa7a02e34c3ddd2c29d2cf7d9782304d548
ghostfart <g.b.maitland@gmail.com>
2016-04-17 12:59:06 +1200

    Add implementation of Forecast Adapter to fake data.

--- app/src/main/java/nz/co/maitech/sunshine/MainActivityFragment.java
+++ app/src/main/java/nz/co/maitech/sunshine/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,13 +19,22 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        String fakeData[] = new String[6];
-        fakeData[0] = new String("Today - Sunny - 88 / 63");
-        fakeData[1] = new String("Tomorrow - Foggy - 70 / 46");
-        fakeData[2] = new String("Wednesday - Cloudy - 72 / 63");
-        fakeData[3] = new String("Thursday - Rainy - 64 / 51");
-        fakeData[4] = new String("Friday - Foggy - 70 / 46");
-        fakeData[5] = new String("Saturday - Sunny - 76 / 68");
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        String weekForecast[] = new String[6];
+        weekForecast[0] = new String("Today - Sunny - 88 / 63");
+        weekForecast[1] = new String("Tomorrow - Foggy - 70 / 46");
+        weekForecast[2] = new String("Wednesday - Cloudy - 72 / 63");
+        weekForecast[3] = new String("Thursday - Rainy - 64 / 51");
+        weekForecast[4] = new String("Friday - Foggy - 70 / 46");
+        weekForecast[5] = new String("Saturday - Sunny - 76 / 68");
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
